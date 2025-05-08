2b105ababc3bcdba0c24c9bd9d8b3f95a6753943
longd-dev <longduong325@gmail.com>
2025-05-07 21:47:38 -0700

    test

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,9 +1,6 @@
 import express from "express";
-import {
-  createCheckoutSession,
-  checkoutSuccess,
-} from "../controllers/payment.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { checkoutSuccess, createCheckoutSession } from "../controllers/payment.controller.js";
 
 const router = express.Router();
 
