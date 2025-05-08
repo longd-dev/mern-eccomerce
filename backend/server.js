f504a24a054d2b97d6be7e622913a13b2a72a190
longd-dev <longduong325@gmail.com>
2025-05-07 21:26:52 -0700

    spelling errors

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "1mb" })); // allows you to parse the body of the request
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -38,6 +38,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.listen(PORT, () => {
-	console.log("Server is running on http://localhost:" + PORT);
-	connectDB();
+		console.log("Server is running on http://localhost:" + PORT);
+		connectDB();
 });
